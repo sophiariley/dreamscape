11fe83d245f3d4c867a8a14d6195940ccda0a98d
Sophia Riley <riley.e.sophia@gmail.com>
2022-10-18 20:10:47 -0400

    wheee

--- dummyCode.js
+++ dummyCode.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 console.log("This is dummy code. It's called 'dummy code' because it's useless.");
-=======
 console.log('Dylans edit, learned this git stuff a long time ago man');
->>>>>>> dylan-branch-test
