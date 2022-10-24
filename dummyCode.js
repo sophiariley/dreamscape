ba99de4810bd1b8246b95987d77e165de61efc9d
Chris Nelson <77455375+chrisnelson01@users.noreply.github.com>
2022-10-23 22:47:43 -0400

    added edit

--- dummyCode.js
+++ dummyCode.js
@@ -2,3 +2,4 @@ console.log("This is dummy code. It's called 'dummy code' because it's useless."
 console.log('Mias edit sheeeesh');
 console.log('Dylans edit, learned this git stuff a long time ago man');
 console.log("This is Sophia's edit.");
+console.log("This is Chris' edit");
