e688a2ee680587f4015d2292df40fc91d2a64ffb
Avalee Jacobs <avalee@email.sc.edu>
2022-10-25 15:00:25 -0400

    commit edit

--- dummyCode.js
+++ dummyCode.js
@@ -3,3 +3,4 @@ console.log('Mias edit sheeeesh');
 console.log('Dylans edit, learned this git stuff a long time ago man');
 console.log("This is Sophia's edit.");
 console.log("This is Chris' edit");
+console.log("This is Ava's edit");
