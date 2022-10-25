3ea6fd5b1ede54f270a93cabdcd78d76032bd90b
Sophia Riley <riley.e.sophia@gmail.com>
2022-10-25 19:16:01 -0400

    skeleton

--- dummyCode.js
+++ dummyCode.js
@@ -1,3 +1,4 @@
 console.log("This is dummy code. It's called 'dummy code' because it's useless.");
 console.log('Dylans edit, learned this git stuff a long time ago man');
 console.log("This is Sophia's edit.");
+
