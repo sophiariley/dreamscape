67256abe17d2c372d013e4fd1170092021262393
Sophia Riley <riley.e.sophia@gmail.com>
2022-11-08 16:46:14 -0500

    oh my god

--- dummyCode.js
+++
@@ -1,6 +0,0 @@
-console.log("This is dummy code. It's called 'dummy code' because it's useless.");
-console.log('Mias edit sheeeesh');
-console.log('Dylans edit, learned this git stuff a long time ago man');
-console.log("This is Sophia's edit.");
-console.log("This is Chris' edit");
-console.log("This is Ava's edit");
