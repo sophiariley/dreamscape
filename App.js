f3e79da7bdaabcb1994d5beccc9f0fb783b984d8
Sophia Riley <riley.e.sophia@gmail.com>
2022-11-15 00:27:42 -0500

    Fixing merge

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-<<<<<<< HEAD
 import { StyleSheet, Text, View } from 'react-native';
 import { useState, useEffect } from "react";
 import { db } from "./firebase-config";
 import { collection, getDocs } from "firebase/firestore";
-=======
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
->>>>>>> Sophia's-branch
 import Constants from 'expo-constants';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -28,83 +25,6 @@ export default function App() {
     getUsers();
   })
   return (
-<<<<<<< HEAD
-    <View style={styles.container}>
-      {users.map((user) => {
-        return (
-          <View key={user.id}>
-            <Text> Name: {user.name}</Text>
-            <Text> Age: {user.age}</Text>
-            <Text> ID: {user.id}</Text>
-          </View>
-        );
-      })}
-      
-    </View>
-  );
-}
-
-// // ---The different screens of Dreamscape---
-// // Looking into consolidating each screen into a seperate file for cleanliness
-// // This is an idea of the screens to create for the PoC milestone 
-// const LoginScreen = ({ navigation}) => {
-//   // TO DO: Code
-// }
-
-// const ResetPasswordScreen = ({ navigation}) => {
-//   // TO DO: Code
-// }
- 
-// // Unsure how many create account screens we need. Should look into consolidating them into one thing
-// const CreateAccountScreen1 = ({ navigation}) => {
-//   // TO DO: Code
-// }
-
-// const CreateAccountScreen2 = ({ navigation}) => {
-//   // code login screen here
-// }
-
-// const CreateAccountScreen3 = ({ navigation}) => {
-//   // TO DO: Code
-// }
-// //----------------------------------------------------------------------------------------------------
-
-// const HomeScreen = ({ navigation}) => {
-//   // TO DO: Code
-// }
-
-// const ProfileScreen = ({ navigation}) => {
-//   // TO DO: Code
-// }
-
-// const ExploreScreen = ({ navigation}) => {
-//   // TO DO: Code
-// }
-
-// const CreatePostScreen = ({ navigation}) => {
-//   // TO DO: Code
-// }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-  },
-  swipes: {
-    flex: 1,
-    padding: 10,
-    paddingTop: 8,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.29,
-    shadowRadius: 4.65,
-    elevation: 7,
-  },
-}) 
-=======
     <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
@@ -139,4 +59,3 @@ const styles = StyleSheet.create({
   }
 }) 
 
->>>>>>> Sophia's-branch
