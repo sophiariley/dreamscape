a3b7b5e7ed5e115ce62dfc47188b46e8b78315fc
Sophia Riley <riley.e.sophia@gmail.com>
2023-01-26 14:00:17 -0500

    this isn't working </3

---
+++ test/LoginScreen.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import LoginScreen from './LoginScreen';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<LoginScreen />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
